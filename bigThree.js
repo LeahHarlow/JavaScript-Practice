23fd271fdd5b2013f5df533835c8f7c9a1b36bf5
Leah Hamilton <leahhamilton@rodrigos-MacBook-Pro.local>
2022-05-09 15:28:41 -0700

    it isnt the prettiest but it works finally

--- bigThree.js
+++ bigThree.js
@@ -1,16 +1,21 @@
+
 function findThreeLargestNumbers(array) {
   if(array.length < 3){
 		return -1
 	}
-	let bigThree = [null,null,null];
+	let bigThree = [-1000,-1000,-1000];
 	for(let i = 0; i < array.length; i++){
-		if(array[i] === null || array[i] > bigThree[2]){
+		if(array[i] >= bigThree[2]){
+			bigThree[0] = bigThree[1]
+			bigThree[1] = bigThree[2]
 			bigThree[2] = array[i];
 		}
-		if(array[i] === null || array[i] > bigThree[1]){
+		else if(array[i] >= bigThree[1]){
+			bigThree[0] = bigThree[1]
+			bigThree[1] = bigThree[2]
 			bigThree[1] = array[i];
 		}
-		if(array[i] === null ||array[i] > bigThree[0]){
+		else if(array[i] >= bigThree[0]){
 			bigThree[0] = array[i];
 		}
 	}
@@ -18,4 +23,8 @@ function findThreeLargestNumbers(array) {
 }
 
 
-console.log(findThreeLargestNumbers([-1,2,57,36,22,10]))
+console.log(findThreeLargestNumbers([-1,2,57,36,22,10])) //[22,36,57]
+
+console.log(findThreeLargestNumbers([57,10])) //-1
+
+console.log(findThreeLargestNumbers([346,-1,200,573])) //[22,36,57]

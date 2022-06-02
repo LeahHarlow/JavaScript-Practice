e87f99a1ceabee900556d6cb8e5a2e856d262989
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-06-02 16:56:57 -0700

    added notes and big O

--- anagram.js
+++ anagram.js
@@ -2,9 +2,11 @@
 //you can assume that there will be only valid, lowercase inputs of two strings.
 
 function validAnagram(str1, str2) {
+  //if the strings are different lengths fail them immediatly
   if (str1.length !== str2.length) {
     return false;
   }
+  //set up frequency counters and populate them
   let str1Frequency = {};
   let str2Frequency = {};
   for (let letter of str1) {
@@ -13,6 +15,7 @@ function validAnagram(str1, str2) {
   for (let char of str2) {
     str2Frequency[char] = ++str2Frequency[char] || 1;
   }
+  //iterate over the frequency counters and see if they are matching
   for(let key in str1Frequency){
     if (str1Frequency[key] !== str2Frequency[key]) {
       return false;
@@ -27,3 +30,7 @@ console.log("expected true and got: ", validAnagram("", ""))
 console.log("expected true and got: ", validAnagram("aabbaa", "ababaa"))
 console.log("expected false and got: ", validAnagram("zuma", "uma"))
 console.log("expected false and got: ", validAnagram("money", "monie"))
+
+
+//time complexity => O(n)
+//space complexity => O(1)

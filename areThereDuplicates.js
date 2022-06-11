faec7e2c4f6c56adb757f1acdc56e8fa958d8d57
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-06-11 12:37:47 -0700

    remember that the pointer solution must be sorted to work, otehrwise the frequency counter makes more sense

--- areThereDuplicates.js
+++ areThereDuplicates.js
@@ -1,26 +1,17 @@
-//Implement a function called areThereDuplicates that accepts a variable number of aguements and checks whether there are any duplicates among the arguments passed in.
-
-// I need to iterate over all fo the arguements somehow and check their frequency
+//Implement a function called areThereDuplicates that accepts a variable number of aguements and checks whether there are any duplicates among the arguments passed in. Return true if there are duplicates and false if there are not.
 
 const areThereDuplicates = (...args) => {
   //spreading into an array for simple iteration
   let inputs = [...args];
-  console.log('ARGS', inputs)
-
   //defining where I will track frequencies
   let frequencies = {};
-
   //populating my frequencies object
   for (let i = 0; i < inputs.length; i++) {
     let char = inputs[i];
     frequencies[char] = ++frequencies[char] || 1;
   }
-
-  console.log('FREQUENCY TABLE', frequencies)
-
   //iterate over the object keys for frequency and if its value is > 1 return false otherwise return true;
   for (let key in frequencies) {
-    console.log('KEY', key)
     if (frequencies[key] > 1) {
       return true;
     }
@@ -33,3 +24,23 @@ console.log('expected false and got:', areThereDuplicates(1, 2, 3, 4));
 console.log('expected false and got:', areThereDuplicates('a', 'b', 'c'));
 console.log('expected true and got:', areThereDuplicates(1, 2, 3, 4, 1));
 console.log('expected true and got:', areThereDuplicates(-2, -3, 4, 0, -3, 4, 4));
+
+const pointerSolution = (...args) => {
+  // array needs to be sorted for pointers to work;
+  args.sort((a,b) => a> b);
+  let start = 0;
+  let next = 1;
+  while( next !== args.length-1){
+    if(args[start] === args[next]){
+      return true;
+    }
+    if(args[start] !== args[next]){
+      start++
+      next++
+    }
+  }
+  return false;
+}
+
+console.log('expected false and got:', pointerSolution(1, 2, 3, 4));
+console.log('expected true and got:', pointerSolution(1,2,3,3,4));

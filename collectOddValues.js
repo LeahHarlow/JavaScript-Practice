ecccc3dcb4219bfd7f244016b997162da8b9a69b
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-06-21 16:10:14 -0700

    another solutions to collectOddValues using pure recursion without a helper function

--- collectOddValues.js
+++ collectOddValues.js
@@ -20,11 +20,31 @@ const collectOddValues = (array) => {
   return result;
 };
 
+const pureRecursion = (array) => {
+  //result will be rewritten but its ok
+  let result = [];
+  //basecase
+  if(!array.length) return [];
+  //push the odd numbers onto result
+  if(array[0] % 2 !== 0){
+    result.push(array[0])
+  }
+  // this is where we're building out the stack until the array is finally []
+  result = result.concat(pureRecursion(array.slice(1)));
+  //stack collapses in on itself and returns fully concatonated result
+  return result;
+}
+
 //tests
 console.log('extected [1,3,5] and got:', collectOddValues([1, 2, 3, 4, 5]));
 console.log('extected [] and got:', collectOddValues([2,4,6]));
 console.log('extected [] and got:', collectOddValues([]));
 console.log('extected [1,7,3,9] and got:', collectOddValues([1, 2, 7, 3, 4, 9]));
+
+console.log('extected [1,3,5] and got:', pureRecursion([1, 2, 3, 4, 5]));
+console.log('extected [] and got:', pureRecursion([2,4,6]));
+console.log('extected [] and got:', pureRecursion([]));
+console.log('extected [1,7,3,9] and got:',pureRecursion([1, 2, 7, 3, 4, 9]));
 
 //time complexity O(n)
 

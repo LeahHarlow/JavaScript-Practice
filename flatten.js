97c12f911a30cd72e1f6d34eee60af0ac018629d
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-06-25 19:35:56 -0700

    solution

--- flatten.js
+++ flatten.js
@@ -1,25 +1,27 @@
 //write a function that takes an array of arrays and returns one flattened array. Use recursion.
 
 //define flattened array
-//if inputArray.length = 0; return flattened array;
-//else if inputArray[0] is a number concatinate it onto flattened array
-//slice the first index off of input array
+//iterate over the input array
+// if we come across a nezted array at any index
+//call flatten again on input array[i] and concatonate that onto the flattened array
+// otherwise if its a number just push it onto flattened array
+// then return it
 
 const flatten = (inputArray) => {
-   let flattenedArray = [];
-   for(let i = 0; i < inputArray.length; i++){
-     if(Array.isArray(inputArray[i])){
-       flattenedArray = flattenedArray.concat(flatten(inputArray[i]));
-      }
-      else{
-        flattenedArray.push(inputArray[i]);
-      }
-   }
-   return flattenedArray;
-}
+  let flattenedArray = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    if (Array.isArray(inputArray[i])) {
+      flattenedArray = flattenedArray.concat(flatten(inputArray[i]));
+    } else {
+      flattenedArray.push(inputArray[i]);
+    }
+  }
+  return flattenedArray;
+};
 
 //tests:
-console.log('expected [1,2,3,4]', flatten([1,[2,3],4]));
-console.log('expected [1,2,3]', flatten([[1],[2],[3]]));
-console.log('expected [1,2,3,4,5]', flatten([[1,[2,3],4],5]));
+console.log('expected [1,2,3,4]', flatten([1, [2, 3], 4]));
+console.log('expected [1,2,3]', flatten([[1], [2], [3]]));
+console.log('expected [1,2,3,4,5]', flatten([[1, [2, 3], 4], 5]));
 console.log('expected [13]', flatten([[[[13]]]]));
+

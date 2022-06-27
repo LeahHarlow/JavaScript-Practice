48763494e7f9cd339c3619bb6d6d545ea888ad6d
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-06-27 16:17:57 -0700

    optimized better bubble sort with swap or no swap

--- bubbleSort.js
+++ bubbleSort.js
@@ -22,14 +22,17 @@ const naiveBubbleSort = (array) => {
 console.log('expected [1,2,4,5] and got:', naiveBubbleSort([4, 2, 1, 5]));
 
 const betterBubbleSort = (array) => {
+  let noSwap = true;
   for (let i = array.length; i > 0; i--) {
     for (let j = 0; j < i - 1; j++) {
       if (array[j] > array[j + 1]) {
         let temp = array[j];
         array[j] = array[j + 1];
         array[j + 1] = temp;
+        noSwap = false;
       }
     }
+    if(noSwap) break;
   }
   return array;
 };

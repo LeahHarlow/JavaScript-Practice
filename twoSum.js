22a6878cbbd91b2d45d5b64d4586db46c21f42e4
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-07-03 10:21:22 -0700

    map solution to twoSum

--- twoSum.js
+++ twoSum.js
@@ -3,21 +3,26 @@
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 // You can return the answer in any order.
+//pseudocode:
+//create map
+//loop over array
+//figure out what each number would need to add up to the target
+//chck to see if its in the map
+//if it is, return the index of the complementary number from the map and i
+//if its not, add it to the map
+//a the very end return an empty array if no sum can be found
 
-//unoptimized, have one loop over and other loop over and
-
-const twoSum = (nums, target) => {
-  var map = new Map();
-
-  for(var i = 0; i<nums.length; i++) {
-      var num = nums[i];
-      if(map.get(num) === undefined) map.set(target-num, i);
-      else return [map.get(num), i];
+const twoSum = function (nums, target) {
+  let map = new Map();
+  for(let i = 0; i < nums.length; i++){
+    let complement = target - nums[i];
+    if(map.has(complement)) return [map.get(complement), i];
+    map.set(nums[i],i); ///two args, key and value
   }
-  return []
+  return [];
 };
 
 console.log('Expected [0,3] and got:', twoSum([1, 2, 7, 4], 5));
 console.log('Expected [] and got:', twoSum([1, 2, 7, -3], 1));
 console.log('Expected [0,5] and got:', twoSum([1, 2, 7, 6, 8, 4], 5));
-console.log('Expected [0,1] and got:', twoSum([3,3], 6));
+console.log('Expected [0,1] and got:', twoSum([3, 3], 6));

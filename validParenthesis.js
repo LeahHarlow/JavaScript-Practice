17e5faaeb5269f9c4708c47ceac5980faa38cffe
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-07-03 18:40:41 -0700

    finally solved it

--- validParenthesis.js
+++ validParenthesis.js
@@ -22,35 +22,28 @@
 const validParens = (s) => {
   if (!s.length || s.length % 2 !== 0) return false;
   let validPairs = {
-    '(': ')',
-    '{': '}',
-    '[': ']',
+    ')': '(',
+    '}': '{',
+    ']': '[',
   };
 
+  //get all the open brackets here
   let openStack = [];
-  let closedStack = [];
 
-  for(let i = 0; i < s.length; i++){
-    if(s[i] === '(' || s[i] === '[' || s[i] === '{'){
-      openStack.push(s[i])
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
+      openStack.push(s[i]);
     }
-    else{
-      closedStack.push(s[i]);
+    //if its closing see if there is a valid open one on the stack ready to line up with it. and return false if not.
+    else if (validPairs[s[i]] !== openStack.pop()) {
+      return false;
     }
   }
-  console.log("CLOSED",closedStack)
-  console.log("OPEN",openStack)
-  while(closedStack.length){
-    let open = openStack.pop();
-    let closed = closedStack.pop();
-    if(validPairs[open] !== closed) return false;
-  }
-  return true;
+  //if the stack array has all been popped off then we know it was all symetrical
+  return !openStack.length
 };
 
-
-
-console.log('Expected true and got:', validParens('{}'))
-console.log('Expected true and got:', validParens('{}[]()'))
-console.log('Expected false and got:',validParens('{]'))
-console.log('Expected false and got:',validParens('([)]'))
+console.log('Expected true and got:', validParens('{}'));
+console.log('Expected true and got:', validParens('{}[]()'));
+console.log('Expected false and got:', validParens('{]'));
+console.log('Expected false and got:', validParens('([)]'));

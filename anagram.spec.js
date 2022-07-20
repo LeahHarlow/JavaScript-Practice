56ffdc79a08ed6dd46b839a6cbe526c54f374399
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-07-20 15:39:36 -0700

    added more tests

--- anagram.spec.js
+++ anagram.spec.js
@@ -1,5 +1,14 @@
 const validAnagram = require('./anagram');
 
 test('tests zuma and umaz expecting true', () => {
-  expect(validAnagram("zuma", "umaz")).toBe(true);
+  expect(validAnagram('zuma', 'umaz')).toBe(true);
+});
+test('tests empty string expecting true', () => {
+  expect(validAnagram('', '')).toBe(true);
+});
+test('tests zuma and uma expecting false', () => {
+  expect(validAnagram('zuma', 'uma')).toBe(false);
+});
+test('tests money and monie expects false', () => {
+  expect(validAnagram('money', 'monie')).toBe(false);
 });

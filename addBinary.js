1984b0a2b59137973290e81f54e2fe49d044716b
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-08-01 15:19:56 -0700

    so close damnit

--- addBinary.js
+++ addBinary.js
@@ -1,15 +1,54 @@
 // Given two binary strings a and b, return their sum as a binary string.
 
+var addBinary = function (a, b) {
+  let binarySum = [];
+  let stackA = a.split('');
+  let stackB = b.split('');
+  let remember = false;
 
-
-
-var addBinary = function(a, b) {
-    let binarySum = ''; //concatonate onto then reverse it before returning? save reindexing?
-    let stackA = a.split('');
-    let stackB= b.split('');
-    console.log(stackA)
+  for (let i = 0; i < Math.max(a.length, b.length); i++) {
+    let aChar = stackA.pop();
+    let bChar = stackB.pop();
+    if (aChar === '1' && bChar === '1') {
+      remember = true;
+      binarySum.push('0');
+    }
+    if (aChar === '1' && bChar === '0') {
+      if (remember) {
+        binarySum.push('0');
+      }
+      if (!remember) {
+          binarySum.push('1');
+        }
+    }
+    if (aChar === '0' && bChar === '0') {
+      if (remember) {
+        binarySum.push('1');
+        remember = false;
+      }
+      if (!remember) {
+        binarySum.push('0');
+      }
+    }
+    if (aChar === '0' && bChar === '1') {
+      if (remember) {
+        binarySum.push('0');
+      }
+      if (!remember) {
+        binarySum.push('1');
+      }
+    }
+  }
+  if (remember) binarySum.push('1');
+  return binarySum.reverse().join('');
 };
 
+console.log('expected 100 and got:', addBinary('11', '1')); //adds to 4
+console.log('expected 10101 and got:', addBinary('1010', '1011')); //adds to 21
+///1010
+///1011
+//10101
 
-console.log("expected 100 and got:", addBinary("11", "1")) //adds to 4
-console.log("expected 10101 and got:", addBinary("1010", "1011")) //adds to 21
+////1
+///11
+//100

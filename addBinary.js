d4e09e0a547019db980676b533f44d460b65548c
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-08-01 15:49:53 -0700

    correct answer with notes

--- addBinary.js
+++ addBinary.js
@@ -1,6 +1,6 @@
 // Given two binary strings a and b, return their sum as a binary string.
 
-//first attempt. It works but lets do it better.
+//first attempt. It no work but cant delete it.
 
 // var addBinary = function (a, b) {
 //   let binarySum = [];
@@ -8,16 +8,21 @@
 //   let binarySumB = b.split('');
 //   let remember = false;
 
-//   while (a.length || b.length) {
+//   while (binarySumA || binarySumB) {
 //     let aChar = binarySumA.pop();
 //     let bChar = binarySumB.pop();
 //     if (aChar === '1' && bChar === '1') {
-//       remember = true;
-//       binarySum.push('0');
+//       if (remember) {
+//         binarySum.push('1');
+//       }
+//       if (!remember) {
+//           binarySum.push('1');
+//          remember = false;
+//         }
 //     }
 //     if (aChar === '1' && bChar === '0') {
 //       if (remember) {
-//         binarySum.push('0');
+//         binarySum.push('1');
 //       }
 //       if (!remember) {
 //           binarySum.push('1');
@@ -47,24 +52,32 @@
 
 const addBinary = (a, b) => {
     //start both strings at the end.
-    let currentAIndex = a.length - 1
-    let currentBIndex = b.length - 1
-    const binarySum = []
-    let remember = 0
+    let currentAIndex = a.length - 1;
+    let currentBIndex = b.length - 1;
+    //push on without reindexing then easier to reverse;
+    const binarySum = [];
+    //to show carring the 1
+    let remember = 0;
 
+    //as long as one of them has length
     while (a[currentAIndex] || b[currentBIndex]) {
-        const sum = Number(a[currentAIndex] || 0) + Number(b[currentBIndex] || 0) + remember
+        //convert the strings to numbers and add either 1 or 0, the || 0 also keeps us from hitting invalid if one strign is shorter + remember will show if we're carrying a 1 or not.
+        const sum = Number(a[currentAIndex] || 0) + Number(b[currentBIndex] || 0) + remember;
+        //if they add up to 1 or 0 just push it on
         if (sum < 2) {
             binarySum.push(sum)
+            //reset remember to 0 bc it would have been added.
             remember = 0
         } else {
+            //say they are two 1's we want to subtract it to zero and push that on but remember to carry the 1
+            //if they were three 1's we want to subtract it to 1 and push that on then keep remember as 1
             binarySum.push(sum - 2)
             remember = 1
         }
         currentAIndex--
         currentBIndex--
     }
-
+    //If we have remember left over than push that on
     if (remember) binarySum.push(remember)
 
     return binarySum.reverse().join('')

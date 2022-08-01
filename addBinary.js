69432e18261c07267f913eda26d5d6095eb7f4a7
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-08-01 15:33:58 -0700

    nope

--- addBinary.js
+++ addBinary.js
@@ -1,46 +1,73 @@
 // Given two binary strings a and b, return their sum as a binary string.
 
-var addBinary = function (a, b) {
-  let binarySum = [];
-  let stackA = a.split('');
-  let stackB = b.split('');
-  let remember = false;
+//first attempt. It works but lets do it better.
 
-  for (let i = 0; i < Math.max(a.length, b.length); i++) {
-    let aChar = stackA.pop();
-    let bChar = stackB.pop();
-    if (aChar === '1' && bChar === '1') {
-      remember = true;
-      binarySum.push('0');
-    }
-    if (aChar === '1' && bChar === '0') {
-      if (remember) {
-        binarySum.push('0');
-      }
-      if (!remember) {
-          binarySum.push('1');
+// var addBinary = function (a, b) {
+//   let binarySum = [];
+//   let binarySumA = a.split('');
+//   let binarySumB = b.split('');
+//   let remember = false;
+
+//   while (a.length || b.length) {
+//     let aChar = binarySumA.pop();
+//     let bChar = binarySumB.pop();
+//     if (aChar === '1' && bChar === '1') {
+//       remember = true;
+//       binarySum.push('0');
+//     }
+//     if (aChar === '1' && bChar === '0') {
+//       if (remember) {
+//         binarySum.push('0');
+//       }
+//       if (!remember) {
+//           binarySum.push('1');
+//         }
+//     }
+//     if (aChar === '0' && bChar === '0') {
+//       if (remember) {
+//         binarySum.push('1');
+//         remember = false;
+//       }
+//       if (!remember) {
+//         binarySum.push('0');
+//       }
+//     }
+//     if (aChar === '0' && bChar === '1') {
+//       if (remember) {
+//         binarySum.push('0');
+//       }
+//       if (!remember) {
+//         binarySum.push('1');
+//       }
+//     }
+//   }
+//   if (remember) binarySum.push('1');
+//   return binarySum.reverse().join('');
+// };
+
+const addBinary = (a, b) => {
+    //start both strings at the end.
+    let currentAIndex = a.length - 1
+    let currentBIndex = b.length - 1
+    const binarySum = []
+    let remember = 0
+
+    while (a[currentAIndex] || b[currentBIndex]) {
+        const sum = Number(a[currentAIndex] || 0) + Number(b[currentBIndex] || 0) + remember
+        if (sum < 2) {
+            binarySum.push(sum)
+            remember = 0
+        } else {
+            binarySum.push(sum - 2)
+            remember = 1
         }
+        currentAIndex--
+        currentBIndex--
     }
-    if (aChar === '0' && bChar === '0') {
-      if (remember) {
-        binarySum.push('1');
-        remember = false;
-      }
-      if (!remember) {
-        binarySum.push('0');
-      }
-    }
-    if (aChar === '0' && bChar === '1') {
-      if (remember) {
-        binarySum.push('0');
-      }
-      if (!remember) {
-        binarySum.push('1');
-      }
-    }
-  }
-  if (remember) binarySum.push('1');
-  return binarySum.reverse().join('');
+
+    if (remember) binarySum.push(remember)
+
+    return binarySum.reverse().join('')
 };
 
 console.log('expected 100 and got:', addBinary('11', '1')); //adds to 4

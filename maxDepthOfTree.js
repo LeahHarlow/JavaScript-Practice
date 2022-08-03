b2fc4f79abb196d1bed35cce116f54aef2326d10
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-08-03 09:25:39 -0700

    by jove were starting to understand trees

--- maxDepthOfTree.js
+++ maxDepthOfTree.js
@@ -2,7 +2,62 @@
 
 // A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
-var maxDepth = function(root) {
-  return (!root) ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(val) {
+    let newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (val === current.val) return undefined; //no duplicates
+      if (val < current.val) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        } else {
+          current = current.right;
+        }
+      }
+    }
+  }
+}
+
+const tree = new BinaryTree();
+tree.insert(3)
+tree.insert(9)
+tree.insert(20)
+tree.insert(15)
+tree.insert(7)
+
+const maxDepth = function (root) {
+  if (!root) {
+    return 0;
+  } else {
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+  }
 };
 
+const maxDepthOpt = function (root) {
+  return !root ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
+
+console.log(maxDepth(tree.root));

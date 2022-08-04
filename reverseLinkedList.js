08fb503e77078ed14855d48317488fd8ea9ee88d
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-08-04 13:54:43 -0700

    solution

--- reverseLinkedList.js
+++ reverseLinkedList.js
@@ -1,3 +1,14 @@
 // Runtime: 63 ms, faster than 97.20% of JavaScript online submissions for Reverse Linked List.
 // Memory Usage: 44.6 MB, less than 23.47% of JavaScript online submissions for Reverse Linked List.
 
+const reverseList = (head) => {
+  let prev = null;
+
+   while(head){
+       let next = head.next;
+       head.next = prev;
+       prev = head;
+       head = next;
+   }
+   return prev;
+}

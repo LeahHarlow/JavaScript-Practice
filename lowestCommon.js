8120bdb229a5831bfb6c437f7573837856712954
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-08-10 22:15:11 -0700

    que

--- lowestCommon.js
+++ lowestCommon.js
@@ -7,29 +7,28 @@
 // Output: 6
 // Explanation: The LCA of nodes 2 and 8 is 6.
 
-
-var lowestCommonAncestor = function(root, nodeOne, nodeTwo) {
-  let lca = root
+const lowestCommonAncestor = function (root, nodeOne, nodeTwo) {
+  let lca = root;
 
   while (lca) {
-      // if both nodes are less than the root it means
-      // that they are both on the left side of the tree.
-      // Move the root to the left.
-      if (nodeOne.val < lca.val && nodeTwo.val < lca.val) {
-          lca = lca.left
-          continue
-      }
+    // if both nodes are less than the root it means
+    // that they are both on the left side of the tree.
+    // Move the root to the left.
+    if (nodeOne.val < lca.val && nodeTwo.val < lca.val) {
+      lca = lca.left;
+      continue;
+    }
 
-      // if both nodes are greater than the root it means
-      // that they are both on the right side of the tree.
-      // Move the root to the right.
-      if (nodeOne.val > lca.val && nodeTwo.val > lca.val) {
-          lca = lca.right
-          continue
-      }
+    // if both nodes are greater than the root it means
+    // that they are both on the right side of the tree.
+    // Move the root to the right.
+    if (nodeOne.val > lca.val && nodeTwo.val > lca.val) {
+      lca = lca.right;
+      continue;
+    }
 
-      return lca
+    return lca;
   }
 
-  return undefined
+  return undefined;
 };

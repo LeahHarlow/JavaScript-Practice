8212a6fac793a1bca5dbfab40e33bcbe9f8c94d4
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-08-25 12:58:18 -0700

    finally got the first part

--- insertInterval.js
+++ insertInterval.js
@@ -8,25 +8,25 @@ const insertInterval = (intervals, newInt) => {
   let newIntervals = [];
   for(let i = 0; i < intervals.length; i++){
     let currentInt = intervals[i];
-    if(currentInt[0] > newInt[0] && currentInt[1] < newInt[1]){
-      intervals.shift(newInt);
-      console.log(intervals)
-      return intervals;
+    if(newInt[1] < currentInt[0]) {
+      intervals.unshift(newInt);
+      return intervals
     }
-    if(currentInt[0] < newInt[0] && currentInt[1] > newInt[1]){
-      newIntervals.push(currentInt);
-      console.log(newIntervals)
-    }
-    if(currentInt[0] < newInt[0] && currentInt[1] < newInt[1]){
-      newIntervals.push([currentInt[0], newInt[1]]);
-      console.log(newIntervals)
-    }
-    if(currentInt[0] > newInt[0] && currentInt[1] > newInt[1]){
-      newIntervals.push([currentInt[0], newInt[1]]);
-      console.log(newIntervals)
-    }
+    // if(currentInt[0] < newInt[0] && currentInt[1] > newInt[1]){
+    //   newIntervals.push(currentInt);
+    //   console.log(newIntervals)
+    // }
+    // if(currentInt[0] < newInt[0] && currentInt[1] < newInt[1]){
+    //   newIntervals.push([currentInt[0], newInt[1]]);
+    //   console.log(newIntervals)
+    // }
+    // if(currentInt[0] > newInt[0] && currentInt[1] > newInt[1]){
+    //   newIntervals.push([currentInt[0], newInt[1]]);
+    //   console.log(newIntervals)
+    // }
   }
-  return newIntervals; 
+  console.log(newIntervals);
+  return newIntervals;
 }
 
 // const insertInterval = (intervals, newInt) => {
@@ -38,8 +38,8 @@ const insertInterval = (intervals, newInt) => {
 
 
 
-
-console.log('expected [[1,5],[6,9]] and got: ', insertInterval([[1,3],[6,9]],[2,5]))
-console.log('expected [[1,2],[3,10],[12,16]] and got: ', insertInterval([[1,2],[3,5],[6,7],[8,10],[12,16]],[4,8]))
+console.log('expected [[2,5],[6,9],[10,11]] and got: ', insertInterval([[6,9],[10,11]],[2,5]))
+// console.log('expected [[1,5],[6,9]] and got: ', insertInterval([[1,3],[6,9]],[2,5]))
+// console.log('expected [[1,2],[3,10],[12,16]] and got: ', insertInterval([[1,2],[3,5],[6,7],[8,10],[12,16]],[4,8]))
 
 
